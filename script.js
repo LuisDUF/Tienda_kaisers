723756563e6e35ebe9b54b755dcd9a7032156e0d
LuisDUF <luis.duran.farias.04@gmail.com>
2025-03-13 09:24:43 -0600

    cosas

---
+++ script.js
@@ -0,0 +1,17 @@
+const instrumentos = {
+    piano: [
+        {name:"Grand piano", color:"Negro", cantidad: 4, precio: 1000000},
+        {name:"Piano vertical",color: "Blanco", cantidad: 6, precio: 500000},
+        {name:"Piano electrónico", color: "Cafe", cantidad: 3, precio: 3000}
+      ],
+      Guitarra: [
+        {name:"Acustica", color:"Cafe", cantidad: 7, precio: 10000},
+        {name:"Electrica",color: "Roja", cantidad: 5, precio: 20000},
+        {name:"Electro acustica", color: "Cafe", cantidad: 4, precio: 15000}
+      ],
+      Flauta: [
+        {name:"Pan", color:"Blanco", cantidad: 10, precio: 200},
+        {name:"Transversal",color: "Negro", cantidad: 5, precio: 2000},
+        {name:"Pan", color: "Cafe", cantidad: 3, precio: 500}
+      ]
+};
