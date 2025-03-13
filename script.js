7e22fe9aa25194032d31cf037f1a9bb52445163a
LuisDUF <luis.duran.farias.04@gmail.com>
2025-03-13 13:19:23 -0600

    CAMBIOS AL INDEX

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 
 
-const instrumentos = {
-    piano: [
-        {name:"Grand piano", color:"Negro", cantidad: 4, precio: 1000000},
+
+    const watches = [
+        {name:"Hamilton Jazzmaster", color:"Silver", cantidad: 4, precio: 24190, img: "rel1.jpg"},
         {name:"Piano vertical",color: "Blanco", cantidad: 6, precio: 500000},
         {name:"Piano electrónico", color: "Cafe", cantidad: 3, precio: 3000}
-      ],
-      Guitarra: [
-        {name:"Acustica", color:"Cafe", cantidad: 7, precio: 10000},
-        {name:"Electrica",color: "Roja", cantidad: 5, precio: 20000},
-        {name:"Electro acustica", color: "Cafe", cantidad: 4, precio: 15000}
-      ],
-      Flauta: [
-        {name:"Pan", color:"Blanco", cantidad: 10, precio: 200},
-        {name:"Transversal",color: "Negro", cantidad: 5, precio: 2000},
-        {name:"Pan", color: "Cafe", cantidad: 3, precio: 500}
-      ]
-};
+    ];
 
-function listar_pianos(a){
-    const tbody = document.getElementById("cantidad");
+window.onload = function listar_productos(){
+    const tbody = document.getElementById("tabla-piano");
+    const body = document.getElementById("cuerpo");
 
-    instrumentos.forEach(instrumento => {
+    watches.forEach(watche => {
         let fila = document.createElement("tr");
 
         fila.innerHTML = `
-        <td>${piano.name}</td>
-        <td>${piano.cantidad}</td>
-        <td>${piano.precio}</td>
+        <td><img src="${watche.img}"></td>
+        <td>${watche.name}</td>
+        <td>${watche.cantidad}</td>
+        <td>${watche.precio}</td>
         `;
         tbody.appendChild(fila);
+
+
+        let cucu = document.createElement("div");
+
+        cucu.innerHTML = `
+        <p>${watche.name}</p>
+        `;
+        
+
     })
 
     
 
-document.getElementById("nombre").innerHTML = instrumentos.piano[a].name;
-document.getElementById("precio").innerHTML = instrumentos.piano[a].precio;
+document.addEventListener("DOMContentLoaded", listar_pianos);
 
 }
